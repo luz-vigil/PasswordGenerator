1991fbd8e752f71da6e8b52304eb1c871c5d4f4d
Luz Maria Vigil <luzmaria.vigil@gmail.com>
2020-08-09 21:41:11 -0700

    added passsword criteria for user to select in order to generate password that meets user's requirements

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCasel = document.querySelector("#lowerCase");
+var upperCaseu = document.querySelector("#upperCase");
+var numericn = document.querySelector("#numeric");
+var specialCharacters = document.querySelector("#specialCharacter");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numeric = "0123456789"
+var specialCharacter = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+
 
 // Write password to the #password input
 function writePassword() {
